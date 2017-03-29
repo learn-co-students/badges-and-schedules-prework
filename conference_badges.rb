958e4415954a7dc358ca3ac6ba08e74cfac0b7c9
Alexandra Williams <alexandrawilliams.aiw@gmail.com>
2017-03-29 00:47:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(people)
+  newray = []
+  people.each do |i|
+    newray.push(badge_maker(i))
+  end
+  return newray
+end
+
+
+def assign_rooms(people)
+  newray2 = []
+  people.each_with_index do |i, index|
+    x = "Hello, #{i}! You'll be assigned to room #{index+1}!"
+    newray2.push(x)
+  end
+  return newray2
+end
+
+
+def printer(people)
+  firstray = batch_badge_creator(people)
+  secray = assign_rooms(people)
+  final = firstray.concat(secray)
+  final.each do |i|
+    puts i
+  end
+end
