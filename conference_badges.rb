c479f05d89a90d8e99b7196539dbdfbc6c9e5d9d
Mike Luo <zluo@alumni.berklee.edu>
2017-04-02 19:15:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = Array.new
+  attendees.each{|name| badges.push("Hello, my name is #{name}.")}
+  return badges
+end
+
+def assign_rooms(attendees)
+  assigned = Array.new
+  room = 0
+  attendees.each{|name| assigned.push("Hello, #{name}! You'll be assigned to room #{room += 1}!")}
+  return assigned
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  rooms = assign_rooms(attendees)
+  badges.each{|badge| puts badge}
+  rooms.each{|room| puts room}
+end
