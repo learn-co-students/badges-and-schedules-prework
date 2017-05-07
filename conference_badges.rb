10ca98a7a2bae422f037a019aee903e162d0db18
Jason Tseng <jason.f.tseng@gmail.com>
2017-05-07 23:15:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do |name|
+    badges.push(badge_maker(name))
+  end
+  badges
+end
+
+def assign_rooms(speakers)
+  rooms = [1, 2, 3, 4, 5, 6, 7]
+  messages = []
+  counter = 0
+  for speaker in speakers
+    message = "Hello, #{speaker}! You'll be assigned to room #{rooms[counter]}!"
+    messages.push(message)
+    counter += 1
+  end
+  messages
+end
+
+def printer(array)
+  badges = batch_badge_creator(array)
+  messages = assign_rooms(array)
+  for badge in badges
+    puts badge
+  end
+  for message in messages
+    puts message
+  end
+end 
