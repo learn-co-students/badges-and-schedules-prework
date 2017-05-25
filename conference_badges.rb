fef849a478697b34cb7e59c55c44acac46e3a131
Caitlin Keck <caitlin.keck@gmail.com>
2017-05-25 22:28:27 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  batch = []
+  speakers.each do |name|
+    batch << badge_maker(name)
+  end
+  batch
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index do |name, index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  room_assignments
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |speaker|
+    puts speaker
+  end
+  assign_rooms(speakers).each do |speaker|
+    puts speaker
+  end
+end
