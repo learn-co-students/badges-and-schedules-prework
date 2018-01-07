308078756864eb5b0be628a84a608a8864ce8e85
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2018-01-07 17:21:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array_of_names)
+def batch_badge_creator(array)
   new_array = []
 
-  array_of_names.each do |name|
+  array.each do |name|
     new_array << badge_maker(name)
   end
 
