8dbf190b544d08d2f5b43a1dbb2965f13a66f51c
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 03:47:10 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  batch_arr = []
+  arr.each do |name|
+  batch_arr.push(badge_maker(name))
+ end
+  batch_arr
+end
+
+def assign_rooms(arr)
+  room_assignments = []
+  arr.each_with_index {|value, index| room_assignments.push("Hello, #{value}! You'll be assigned to room #{index + 1}!")}
+  room_assignments
+end
+
+def printer(name)
+ batch_badge_creator(name).each do |badge|
+   puts badge
+end
+
+ assign_rooms(name).each do |room|
+  puts room
+end
+end
