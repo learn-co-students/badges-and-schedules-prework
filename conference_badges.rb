5d9cee85a8d282ca1562eb4d14912bcb865fead7
jessebillard <jessebillard@mac.com>
2018-03-21 16:26:53 -0400

    Badges and Schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |i|
+      new_array.push(badge_maker(i))
+  end
+  new_array
+end
+
+def assign_rooms(array)
+    new_array = []
+    array.each_with_index do |name, i|
+        new_array.push("Hello, #{name}! You'll be assigned to room #{i + 1}!")
+    end
+    new_array
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |i|
+        puts i
+    end
+    assign_rooms(array).each do |x|
+        puts x
+    end 
+end
