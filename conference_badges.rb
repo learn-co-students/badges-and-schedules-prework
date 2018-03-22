0d71e470b3ca41cd6cc7123e60840ea069d9b0d8
Mike C <mikecronin92@gmail.com>
2018-03-22 21:55:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require "benchmark"
+
+clients = %w(Edsger Ada Charles Alan Grace Linus Matz)
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(clients)
+  clients_badge = []
+  clients.each {|client|
+    clients_badge << badge_maker(client)
+  }
+  return clients_badge
+end
+
+def assign_rooms(clients)
+  room_assignments = []
+  clients.each_with_index {|client,index|
+    room_assignments << "Hello, #{client}! You'll be assigned to room #{index +1}!"
+  }
+  return room_assignments
+end
+
+def printer(clients)
+  clients_badge = batch_badge_creator(clients)
+  room_assignments = assign_rooms(clients)
+  clients.each_with_index { |val,index|
+    puts clients_badge[index]
+    puts room_assignments[index]
+    }
+end
+
+puts Benchmark.measure {printer(clients)}
