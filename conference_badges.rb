a47326d3d559c873fc50dd68ef7c305df0ad3ccb
jarretbryan <jarret.bryan@gmail.com>
2018-04-09 23:53:22 -0400

    First two methods completed, need to work on last two

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_array = []
+  names.each do |name|
+    badge_array.push(badge_maker(name))
+  end
+  #alternatively, I coud have done
+  #names.each {|name| badge_array.push(badge_make(name))}
+  badge_array
+end
