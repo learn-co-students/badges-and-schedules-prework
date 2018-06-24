e4401fbf12426de979f1aa49c05eb2e6e9805464
Bryan Teng <bteng1226@gmail.com>
2018-06-24 07:36:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,14 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.each{|x| badge_maker(x)}
+  array.each{|x| puts(badge_maker(x))}
 end
 
 def assign_rooms(array)
-  array.each_with_index{|x,i| "Hello, #{x}! You'll be assigned to room #{i+1}!"}
+  array.each_with_index{|x,i| puts "Hello, #{x}! You'll be assigned to room #{i+1}!"}
 end
 
-def
+def printer
+  batch_badge_creator(array)
+  assign_rooms(array)
+end
