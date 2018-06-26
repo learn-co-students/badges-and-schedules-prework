669fdbe7de220a23ff6f6b2ef74ef69674ec752f
Dina Hafez <dinahafez.8@gmail.com>
2018-06-26 18:59:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each {|attendee| puts attendee}
-  assign_rooms(attendees).each {|attendee| puts attendee}
+  batch_badge_creator(attendees).each {|badge| puts badge}
+  assign_rooms(attendees).each {|assignment| puts assignment}
 end
