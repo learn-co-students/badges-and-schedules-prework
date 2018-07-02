1a0f5f71bab30a6d52e04a211e16e6c0a75914a2
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 21:21:14 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+array = ["Ben", "Ali", "Steve", "Sarah", "David", "Nelly", "Peter", "Jessica"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  badge_array = []
+    name_array.each { |name| badge_array<< badge_maker(name) }
+  badge_array
+end
+
+def assign_rooms(name_array)
+  welcome = []
+  name_array.each_with_index { |name,index|
+  index += 1
+  welcome<< "Hello, #{name}! You'll be assigned to room #{index}!" if index <= 7} 
+  welcome
+end
+
+def printer(name_array)
+    batch_badge_creator(name_array).each { |name| puts name}
+    assign_rooms(name_array).each { |name| puts name}
+end
