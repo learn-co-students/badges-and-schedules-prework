a0696a076d2134d71799a104f7ed06560c871c44
Amalie S <amaliesmidth@gmail.com>
2018-08-23 16:15:26 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+attendees = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+
+def badge_maker(name) 
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  attendees.each do |person|
+    badges = badge_maker(person)
+  end
+  badges
+end
+
+
+def assign_rooms(attendees)
+  
+end
+
+
+
+
+def printer
+  
+end
