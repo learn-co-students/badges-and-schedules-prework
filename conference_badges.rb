8addf128aefebd0dceeea771659852ff7fb77df7
Amalie S <amaliesmidth@gmail.com>
2018-08-24 16:23:47 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,5 @@
+# require 'pry'
+
 attendees = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
 
 def badge_maker(name) 
@@ -14,12 +16,21 @@ end
 
 
 def assign_rooms(attendees)
-  
+  attendees.each_with_index do | person, index |
+    room_assignments =  "Hello, #{person}! You'll be assigned to room #{index + 1}!"
+  end
+  room_assignments
 end
 
 
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  badges.each do | badge |
+    puts badge 
+  room_assignments = assign_rooms(attendees)
+  room_assignments.each do | room_assignment | 
+    puts room_assignment
+  end
+end 
 
 
-def printer
-  
-end
