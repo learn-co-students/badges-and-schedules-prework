7c58a251f3b338cdea34af3b9f7d7004cdfc79dd
github username <github email address>
2018-10-13 19:55:15 +0300

    badge_maker, batch_badge_creater, assign_rooms, and printer methods created

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  i = 0
+
+  while i < attendees.length
+  badges.push("Hello, my name is #{attendees[i]}.")
+  i += 1
+  end
+
+  badges
+
+end
+
+def assign_rooms(attendees)
+  message = []
+  hash = Hash.new
+
+  attendees.each_with_index {|item, index|
+  hash[item] = index
+  message.push("Hello, #{item}! You'll be assigned to room #{index + 1}!")
+}
+
+message
+
+end
+
+def printer(attendees)
+
+batch_badge_creator(attendees).each do |item|
+    puts "#{item}"
+end
+
+assign_rooms(attendees).each do |item|
+    puts "#{item}"
+end
+
+end
