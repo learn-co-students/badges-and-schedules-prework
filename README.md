cfcb0050ff35066a48412c3776f0d135e03fa73c
Tom Rowson <tom.rowson@gmail.com>
2018-10-18 18:36:34 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Badges and Schedules
+ood th# Badges and Schedules
 
 ## Objectives
 
--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array)
+  array.map { |item| 
+  badge_maker(item) }
+end
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map { |speaker, index| "Hello, #{speaker}! You'll be assigned to room #{(index+1)}!"}
+end
+
+def printer(array)
+ batch_badge_creator(array).each {|item| puts item}
+ assign_rooms(array).each {|item2| puts item2 }
+end
+
+
