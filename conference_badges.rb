7c53db306bf01f4630c757a4b5a880611d5f4e03
ko kristin <koberlander115@gmail.com>
2019-02-14 22:13:23 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,14 @@ def batch_badge_creator(name_array)
   counter = 0 
   i = name_array[i]
   while counter < name_array.length
-  puts `Hello, my name is #{name_array[i]}.`
+  puts "Hello, my name is #{name_array[i]}."
   counter += 1
   end
+end
+
+
+def assign_rooms
+end
+
+def printer
 end
