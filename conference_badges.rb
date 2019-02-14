b803f313fa5921c3f5f188cb4f4d59be0cec35e4
ko kristin <koberlander115@gmail.com>
2019-02-14 20:44:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,8 @@
 # Write your code here.
 
-def batch_badge_creator
-  
+def batch_badge_creator(name_array)
+  counter = 0 
+  while counter < name_array.length
+  puts "Hello, my name is #{name_array[counter]}."
+  end
 end
