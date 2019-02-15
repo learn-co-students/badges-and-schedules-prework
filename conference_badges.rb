ad67a1ee37c102890b5a2be5eea7d6ba3ab1370b
ko kristin <koberlander115@gmail.com>
2019-02-15 16:26:10 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,19 +2,22 @@
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 name = "Arel"
+rooms = [1..7]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
+ #take the attendees array and map the "Hello, my name is blah" onto each name in the array
  attendees.map do | attendee |
    badge_maker(attendee)
  end
 end
 
 
-def assign_rooms
+def assign_rooms(attendees)
+  
 end
 
 def printer
