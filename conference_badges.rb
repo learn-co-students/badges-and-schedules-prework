c2e8766ed92e9126012d99baedfae3b6e19ed2d6
ko kristin <koberlander115@gmail.com>
2019-02-16 00:00:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,9 +26,24 @@ def assign_rooms(attendees)
   newArray
 end
 
-def printer
- batch_badge_creator(attendees)
- newArray = []
- newArray = assign_rooms(attendees)
-   puts newArray
+# Question 4
+    # The method `printer` should output first the results of the batch_badge_creator method and then of the assign_rooms method to the screen - this way you can output
+    # the badges and room assignments one at a time.
+    # To make this test pass, make sure you are iterating through your badges and room assignments lists.
+
+        # $stdout is a Ruby global varibale that represents the current standard output.
+        # In this case, the standard output is your terminal screen. This test, then,
+        # is checking to see whether or not your terminal screen receives the correct
+        # printed output.
+
+def printer(attendees)
+# call .each on the return of the batch_badge_creator method 
+ batch_badge_creator(attendees).each do | badge |
+    puts badge
+  end
+ 
+# call .each on the return of the assign_rooms method 
+ assign_rooms(attendees).each do | room |
+   puts room
+ end 
 end
