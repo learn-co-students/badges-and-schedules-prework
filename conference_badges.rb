83b57a36f900e7336c7300c46ae64b5be47b1b67
daniela-sandoval <daniela.sandoval820@gmail.com>
2019-04-13 23:37:17 -0400

    defined conference methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges.push(badge_maker(name))
+  end
+  return badges
+end
+
+def assign_rooms(attendees)
+  list = []
+  room = 1
+  attendees.each do |name|
+    list.push("Hello, #{name}! You'll be assigned to room #{room}!")
+    room += 1
+  end
+  return list
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end
+end
