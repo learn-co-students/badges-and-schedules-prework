c37525ee97b6bc6be671cfda80f19ad5d28296a7
Robert Pedersen <rob@worldcrimeleague.org>
2019-05-01 23:03:58 -0400

    badges done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+
+end# Write your code here.
+
+def batch_badge_creator(name)
+  badge_list = []
+  name.each do |n|
+    badge_list.push("Hello, my name is #{n}.")
+  end
+  badge_list
+end
+
+def assign_rooms(name)
+  array_new = []
+  name.each_with_index { |item , index| array_new.push("Hello, #{item}! You'll be assigned to room #{index + 1}!") }
+return array_new
+end
+
+def printer(array)
+batch_badge_creator(array).each do |b|
+  puts b
+end
+assign_rooms(array).each do |b|
+  puts b
+end
+end
