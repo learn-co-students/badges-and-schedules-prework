d320be2a1529f1ba16a4b25f5e559e98359fffc1
Kurtis Leach <danceflurry4366@gmail.com>
2019-05-10 00:16:27 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator (array)
+  new_array = []
+  array.each do |name|
+    new_array.push(badge_maker(name))
+  end
+  return new_array
+end
+def assign_rooms (array)
+  rooms = []
+  array.each_with_index do |name, room|
+    rooms.push ("Hello, #{name}! You'll be assigned to room #{room+1}!")
+  end
+  return rooms
+end
+def printer (array)
+  badge = batch_badge_creator (array)
+  rooms = assign_rooms (array)
+  badge.each do |info|
+    puts info
+  end
+  rooms.each do |info|
+    puts info
+  end
+end
