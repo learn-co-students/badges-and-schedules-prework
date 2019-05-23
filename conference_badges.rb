9ef7e29734e41e812ce02423374bdd6ae8ad1876
Aaron B <abriscoe2008@gmail.com>
2019-05-23 01:38:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.push([badge_maker])
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  
+
