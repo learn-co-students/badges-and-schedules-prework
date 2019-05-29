8ecbff3c75186cb23361534ad9929ed2c977b283
Dylan Harris <dylanharris23@gmail.com>
2019-05-29 20:53:16 +0000

    Done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is Arel."
+end
+def batch_badge_creator(array)
+  attendees = badges
+end
+def assign_rooms(welcome)
+  attendees = room_assignments
+end
+def printer (array)
+  badge = batch_badge_creator (array)
+  rooms = assign_rooms (array)
+  badge.each do |info|
+    puts info
+  end
+  rooms.each do |info|
+    puts info
+  end
+end
